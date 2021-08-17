b41987afba5af5d1339cb9f538f3c8380fe684ae
geesoon <hiewgs@gmail.com>
2021-08-17 10:42:56 +0800

    Refactor: Putting code into functions

--- blb_concordance.py
+++ blb_concordance.py
@@ -1,18 +1,19 @@
-from os import P_DETACH
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
+import time
 
 
 class BLBConcordance:
     def __init__(self):
         self.browser = self.setWebDriver()
         self.pagesTag = []
+        self.ref = []
+        self.verse = []
         print('---- Blue Letter Bible Concordance -----\n')
 
     def setWebDriver(self):
@@ -25,6 +26,7 @@ class BLBConcordance:
     def getSearchInput(self):
         self.searchTerm = input('Enter search term: ')
 
+    # Check if there is any result to the search term throw if there is none
     def checkResult(self):
         self.browser.get(
             f'https://www.blueletterbible.org/search/search.cfm?Criteria="{self.searchTerm}"&t=KJV#s=s_primary_0_1'
@@ -50,84 +52,111 @@ class BLBConcordance:
             # Append one page to the pagesTag if there is no pagination
             self.pagesTag.append("s_primary_0_1")
 
+    # Split ref - verse into ref, verse
+    def splitRefandVerse(self, verse):
+        parts = verse.split("\n")[1].split("-")
+        self.ref.append(parts[0])
+        self.verse.append(parts[1])
+
     # Get all references from all pagination
     def scrapReferences(self):
+        startTime = time.time()
+
         if (self.checkResult()):
             print('\nScraping references...\n')
-
             self.scrapResultPage()
-            verseSelector = 'div.columns.tablet-2.tablet-order-2.show-for-tablet.text-center > p > a'
-
-            refs = []
 
             for tag in self.pagesTag:
-                url = f'https://www.blueletterbible.org/search/search.cfm?Criteria="{self.searchTerm}"&t=KJV#s={tag}'
 
+                # Go to specific result page
+                url = f'https://www.blueletterbible.org/search/search.cfm?Criteria="{self.searchTerm}"&t=KJV#s={tag}'
                 self.browser.get(url)
+
+                # Wait for result dom to load
                 try:
                     delay = 5  # seconds
                     WebDriverWait(self.browser, delay).until(
-                        EC.visibility_of_all_elements_located((
-                            By.CSS_SELECTOR,
-                            "div.columns.tablet-2.tablet-order-2.show-for-tablet.text-center"
-                        )))
+                        EC.visibility_of_all_elements_located(
+                            (By.CSS_SELECTOR, "div.tools.row.align-middle")))
                 except TimeoutException:
                     print(tag)
 
-                ref = self.browser.find_elements_by_css_selector(verseSelector)
-                for el in ref:
-                    refs.append(el.get_attribute('innerHTML'))
+                # Retrieve all result dom elements
+                verses = self.browser.find_elements_by_css_selector(
+                    "div.tools.row.align-middle")
+
+                # Split and store references and verses
+                for verse in verses:
+                    self.splitRefandVerse(str(verse.text))
 
             print('\nFinished scraping references...\n')
-            return refs
-        return None
 
-    # Get multi-verses by multi-references
-    def getVersesByReferences(self, refs):
-        joinedVerses = self.formatRefs(refs)
-        print(joinedVerses)
+            # Combine references and verses into a dictionary
+            data = {"reference": self.ref, "verse": self.verse}
 
-        print('\nScraping verses...\n')
+            # Print scraping time
+            print(
+                f'----- Scraping time: {round((time.time() - startTime),2)} seconds -----'
+            )
+            return data
+        else:
+            return None
 
-        self.browser.get('https://blueletterbible.org')
-        mvForm = self.browser.find_element_by_id("mvFormWidge")
-        mvText = self.browser.find_element_by_id('mvTextWidge')
-        mvText.send_keys(joinedVerses)
-        mvForm.submit()
+    # def splitRefandVerse(self, verse):
+    #     parts = verse.split("-")
+    #     self.ref.append(parts[0])
+    #     self.verse.append(parts[1])
 
-        # Change the view to multi-verse view
-        try:
-            delay = 20  # seconds
-            WebDriverWait(self.browser, delay).until(
-                EC.visibility_of_element_located(
-                    (By.CSS_SELECTOR, "button#formatButton")))
+    # def scrapReferences(self):
+    #     action = ActionChains(self.browser)
+    #     startTime = time.time()
 
-            self.browser.find_element_by_id('formatButton').click()
-        except TimeoutException:
-            print("Verse loading took too much time!")
+    #     if (self.checkResult()):
+    #         print('\nScraping references...\n')
+    #         self.scrapResultPage()
 
-        # Scrap verses from the verse list
-        allVerses = self.browser.find_elements_by_css_selector(
-            'div.scriptureText')
-        verses = []
-        for verse in allVerses:
-            verseParts = verse.text.split("-")
-            verses.append(verseParts[1])
+    #         for tag in self.pagesTag:
 
-        print('\nFinished scraping verses for all references...\n')
+    #             # Go to specific result page
+    #             url = f'https://www.blueletterbible.org/search/search.cfm?Criteria="{self.searchTerm}"&t=KJV#s={tag}'
+    #             self.browser.get(url)
 
-        return verses
+    #             try:
+    #                 delay = 5  # seconds
+    #                 WebDriverWait(self.browser, delay).until(
+    #                     EC.visibility_of_all_elements_located(
+    #                         (By.CSS_SELECTOR, "div.tools.row.align-middle")))
+    #             except TimeoutException:
+    #                 print(tag)
 
-    # Join the list of reference by ';'
-    def formatRefs(self, refs):
-        return ";".join(refs)
+    #             # Check all verses into clipboard
+    #             selectCheckBoxes = self.browser.find_elements_by_css_selector(
+    #                 "img.copyBox.selected")
+    #             for box in selectCheckBoxes:
+    #                 box.click()
+
+    #             # Split and store references and verses
+    #             verses = self.browser.find_element_by_id(
+    #                 "copyAct").get_attribute(
+    #                     "data-clipboard-text").splitlines()
+    #             for verse in verses:
+    #                 self.splitRefandVerse(verse)
+
+    #         print('\nFinished scraping references...\n')
+
+    #         # Combine references and verses into a dictionary
+    #         data = {"reference": self.ref, "verse": self.verse}
+
+    #         # Print scraping time
+    #         print(
+    #             f'----- Scraping time: {round((time.time() - startTime),2)} seconds -----'
+    #         )
+    #         return data
+    #     else:
+    #         return None
 
     # Output the verses and reference into an excel file
-    def outputToExcel(self, refs, verses):
-        print(f'\nNumber of references scraped: {len(refs)}')
-        print(f'Number of verses scraped: {len(verses)}\n')
-
-        data = {'ref': refs, 'verse': verses}
+    def outputToExcel(self, data):
         df = pd.DataFrame(data)
 
         with pd.option_context('display.max_rows', None, 'display.max_columns',
@@ -149,14 +178,7 @@ class BLBConcordance:
 if __name__ == '__main__':
     concordance = BLBConcordance()
     concordance.getSearchInput()
-    refs = concordance.scrapReferences()
-    if (refs != None):
-        verses = concordance.getVersesByReferences(refs)
-        concordance.outputToExcel(refs, verses)
-
+    res = concordance.scrapReferences()
+    if (res != None):
+        concordance.outputToExcel(res)
     concordance.tearDown()
-
-
-    # TODO
-    # Use copy and paste method instead of scraping directly
-    # Combine multiple searchTerm result in an excel file
