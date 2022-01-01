8236369371315694c2041bd2330acbff9c7df5de
GeeSoon <hiewgs@gmail.com>
2022-01-01 11:28:25 +0800

    Update chrome version, Include TODOs

--- blb_concordance.py
+++ blb_concordance.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
@@ -7,6 +8,11 @@ from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
 
+# TODO:
+# 1. Group reference chapter to determine study range
+# 2. Include key words to exclude
+# 3. Include the options for NT only, OT only, Both
+
 
 class BLBConcordance:
     def __init__(self):
@@ -14,17 +20,17 @@ class BLBConcordance:
         self.pagesTag = []
         self.ref = []
         self.verse = []
-        print('---- Blue Letter Bible Concordance -----\n')
+        print("---- Blue Letter Bible Concordance -----\n")
 
     def setWebDriver(self):
         print("\nSetting up web driver...\n")
-        path = {'.\chromedriver_win32\chromedriver.exe'}
+        path = ".\chromedriver_win32\chromedriver.exe"
         options = Options()
         options.add_argument("--incognito --headless")
         return webdriver.Chrome(path, options=options)
 
     def getSearchInput(self):
-        self.searchTerm = input('Enter search term: ')
+        self.searchTerm = input("Enter search term: ")
 
     # Check if there is any result to the search term throw if there is none
     def checkResult(self):
@@ -37,16 +43,15 @@ class BLBConcordance:
             )
             return True
         except NoSuchElementException:
-            print(f'There is no result for {self.searchTerm}')
+            print(f"There is no result for {self.searchTerm}")
             return False
 
     # Get the result pagination
     def scrapResultPage(self):
-        self.pages = self.browser.find_elements_by_css_selector(
-            '#pageCont_TR > td > p')
-        if (self.pages != []):
+        self.pages = self.browser.find_elements_by_css_selector("#pageCont_TR > td > p")
+        if self.pages != []:
             for button in self.pages:
-                tag = button.find_element_by_tag_name('a').get_attribute('rel')
+                tag = button.find_element_by_tag_name("a").get_attribute("rel")
                 self.pagesTag.append(tag)
         else:
             # Append one page to the pagesTag if there is no pagination
@@ -62,8 +67,8 @@ class BLBConcordance:
     def scrapReferences(self):
         startTime = time.time()
 
-        if (self.checkResult()):
-            print('\nScraping references...\n')
+        if self.checkResult():
+            print("\nScraping references...\n")
             self.scrapResultPage()
 
             for tag in self.pagesTag:
@@ -77,26 +82,29 @@ class BLBConcordance:
                     delay = 5  # seconds
                     WebDriverWait(self.browser, delay).until(
                         EC.visibility_of_all_elements_located(
-                            (By.CSS_SELECTOR, "div.tools.row.align-middle")))
+                            (By.CSS_SELECTOR, "div.tools.row.align-middle")
+                        )
+                    )
                 except TimeoutException:
                     print(tag)
 
                 # Retrieve all result dom elements
                 verses = self.browser.find_elements_by_css_selector(
-                    "div.tools.row.align-middle")
+                    "div.tools.row.align-middle"
+                )
 
                 # Split and store references and verses
                 for verse in verses:
                     self.splitRefandVerse(str(verse.text))
 
-            print('\nFinished scraping references...\n')
+            print("\nFinished scraping references...\n")
 
             # Combine references and verses into a dictionary
             data = {"reference": self.ref, "verse": self.verse}
 
             # Print scraping time
             print(
-                f'----- Scraping time: {round((time.time() - startTime),2)} seconds -----'
+                f"----- Scraping time: {round((time.time() - startTime),2)} seconds -----"
             )
             return data
         else:
@@ -159,26 +167,25 @@ class BLBConcordance:
     def outputToExcel(self, data):
         df = pd.DataFrame(data)
 
-        with pd.option_context('display.max_rows', None, 'display.max_columns',
-                               None):
+        with pd.option_context("display.max_rows", None, "display.max_columns", None):
             print(df)
 
-        outputToExcel = input('\nOutput to an excel file? [Y] / [N] : ')
-        if (outputToExcel == 'Y' or outputToExcel == 'y'):
-            fileName = f'BLB-{self.searchTerm}'
-            df.to_excel(f'{fileName}.xlsx', sheet_name=fileName)
-            print(f'\nFinished output to excel filename: {fileName}.xlsx\n')
+        outputToExcel = input("\nOutput to an excel file? [Y] / [N] : ")
+        if outputToExcel == "Y" or outputToExcel == "y":
+            fileName = f"BLB-{self.searchTerm}"
+            df.to_excel(f"{fileName}.xlsx", sheet_name=fileName)
+            print(f"\nFinished output to excel filename: {fileName}.xlsx\n")
 
     # Destroy webdriver
     def tearDown(self):
         self.browser.close()
-        print('\n----- THE END -----\n')
+        print("\n----- THE END -----\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     concordance = BLBConcordance()
     concordance.getSearchInput()
     res = concordance.scrapReferences()
-    if (res != None):
+    if res != None:
         concordance.outputToExcel(res)
     concordance.tearDown()
